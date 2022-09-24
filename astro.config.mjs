be2f5c16458181d2a0789af01fa1ce7b043cc659
David Vekony <68352412+davidvekony@users.noreply.github.com>
2022-09-24 15:58:30 +0100

    remove React as a dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()]
+  integrations: [image()]
 });
