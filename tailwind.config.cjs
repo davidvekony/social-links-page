921878100d47d231227ebebc28955f2f7f02caec
David Vekony <david.vekony@LEGO.com>
2023-09-01 08:51:45 +0100

    remove scoped CSS from files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"],
+  },
+};
